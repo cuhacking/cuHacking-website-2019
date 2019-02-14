47261bc5049f527e14daf8fd1a2b42569b452cd9
Michael Maxwell <mike.ian.maxwell@gmail.com>
2019-02-14 09:31:42 -0500

    tiny change

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,7 @@ footer.footer
   .content.has-text-centered
     img.footer-logo(src='img/logo_text.png')
     h4 Carleton University
-    h6 1125 Colonel By Dr,
+    h6 1125 Colonel By Dr.
     h6 Ottawa, ON K1S 5B6
     .organizers
       p
